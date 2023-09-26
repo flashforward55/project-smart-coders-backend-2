ef813aaa79a7bd0c72280f35f4cf37f9faf893b7
Anton Styazhkin <rudana089@gmail.com>
2023-09-26 22:26:41 +0300

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -20,12 +20,4 @@ router.post('/register', validateBody(registerValidationSchema), register);
 router.post('/login', validateBody(loginValidationSchema), login);
 router.post('/logout', auth, logout);
 
-/* router.patch(
-  '/user',
-  auth,
-  uploadAvatar,
-  validateBody(updateUserSchema),
-  updateUser
-); */
-
 module.exports = router;
